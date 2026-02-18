e77652709867fd96ab6f42a41fad98ab02f7d998
Jason Hawgood <jason@seavista.co>
2026-02-18 18:08:38 -0500

    Refactor next.config.ts by removing unnecessary blank lines

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  reactStrictMode: true,
-  
-
-
-  
+  reactStrictMode: true
 };
 
 export default nextConfig;
